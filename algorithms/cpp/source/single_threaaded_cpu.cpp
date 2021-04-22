35c21948c3ef866e23fe7973b0ff8cf817576d9f
StrayWarrior <i@straywarrior.com>
2021-04-23 01:45:45 +0800

    fix single_threaded_cpu

--- algorithms/cpp/source/single_threaaded_cpu.cpp
+++ algorithms/cpp/source/single_threaaded_cpu.cpp
@@ -17,8 +17,8 @@ namespace {
 
 class Solution {
 public:
-    vector<int> solve(vector<vector<int>>& tasks) {
-        vector<int> ret = solve_incompletev1(tasks);
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int> ret = solve_v2(tasks);
         return ret;
     }
 
@@ -28,7 +28,14 @@ public:
         int process_time;
     };
 
-    vector<int> solve_incompletev1(vector<vector<int>>& tasks) {
+    using TaskQueueValue = pair<int, int>;
+    struct TaskQueueValueComp {
+        bool operator()(const TaskQueueValue& a, const TaskQueueValue& b) {
+            return a.first > b.first;
+        }
+    };
+
+    vector<int> solve_v1(vector<vector<int>>& tasks) {
         int n = tasks.size();
         vector<Task> sorted_tasks(n);
         for (int i = 0; i < n; ++i) {
@@ -44,20 +51,45 @@ public:
         );
         vector<int> ret;
         priority_queue<int, std::vector<int>, std::greater<int>> runtime_types;
-        // unordered_map<int, vector<pair<int, vector<int, int>>> task_map;
-        // process_time -> task_idx, enqueue_time
-        unordered_map<int, list<pair<int, int>>> task_map;
-        int current_time = 0;
-        int next_idle_time = 0;
+        using TaskQueue = priority_queue<TaskQueueValue,
+                                         vector<TaskQueueValue>,
+                                         TaskQueueValueComp>;
+        unordered_map<int, TaskQueue> task_map;
+        task_map.reserve(20000);
+        int64_t next_idle_time = 0;
         int event_time = 0;
+
+        auto do_schedule_to = [&](int64_t next_enqueue_time) {
+            while (!runtime_types.empty() &&
+                    next_idle_time < next_enqueue_time) {
+                int process_time = runtime_types.top();
+                runtime_types.pop();
+                auto& tasks = task_map[process_time];
+                const auto& task = tasks.top();
+                int task_idx = task.first;
+                int64_t enqueue_time = task.second;
+                tasks.pop();
+                ret.push_back(task_idx);
+                next_idle_time = std::max(next_idle_time, enqueue_time);
+                // std::cout << "schedule at time " << next_idle_time
+                //     << ", task " << task_idx
+                //     << ", process_time: " << process_time << std::endl;
+                next_idle_time += process_time;
+            }
+        };
+
+        bool wait_same_time_tasks = false;
         for (int i = 0; i < n; ++i) {
             const auto& front_task = sorted_tasks[i];
             int event_time = front_task.enqueue_time;
             int process_time = front_task.process_time;
-            std::cout << "add task " << front_task.idx
-                << ", enqueue time: " << event_time
-                << ", process time: " << process_time << std::endl;
-            task_map[process_time].push_back(
+            if (next_idle_time < event_time && wait_same_time_tasks == false) {
+                do_schedule_to(event_time);
+            }
+            // std::cout << "add task " << front_task.idx
+            //     << ", enqueue time: " << event_time
+            //     << ", process time: " << process_time << std::endl;
+            task_map[process_time].push(
                     std::make_pair(front_task.idx, event_time));
             runtime_types.push(process_time);
             int next_enqueue_time = INT_MAX;
@@ -66,33 +98,15 @@ public:
                 if (event_time == next_enqueue_time) {
                     // do not schedule until all the tasks at the same time
                     // are considered
+                    wait_same_time_tasks = true;
                     continue;
+                } else {
+                    wait_same_time_tasks = false;
                 }
-                // do not schedule if cpu is not idle and
-                // there are other tasks to be read.
-                if (next_idle_time > event_time) {
-                    continue;
-                }
-            }
-
-            // try to schedule
-            while (!runtime_types.empty() &&
-                    next_idle_time < next_enqueue_time) {
-                int process_time = runtime_types.top();
-                runtime_types.pop();
-                auto& tasks = task_map[process_time];
-                const auto& task = tasks.front();
-                int task_idx = task.first;
-                int enqueue_time = task.second;
-                tasks.pop_front();
-                ret.push_back(task_idx);
-                next_idle_time = std::max(next_idle_time, enqueue_time);
-                std::cout << "schedule at time " << next_idle_time
-                    << ", task " << task_idx
-                    << ", process_time: " << process_time << std::endl;
-                next_idle_time += process_time;
             }
         }
+        // try to schedule
+        do_schedule_to(INT64_MAX);
         return ret;
     }
 };
@@ -105,11 +119,12 @@ TEST_CASE( "test corectness", "leetcode.cpp.single_threaded_cpu" ) {
     std::vector<TestCase> test_cases {
         TestCase({{1, 2}, {2, 4}, {3, 2}, {4, 1}}, {0, 2, 3, 1}),
         TestCase({{7, 10}, {7, 12}, {7, 5}, {7, 4}, {7, 2}}, {4, 3, 2, 0, 1}),
-        TestCase({{19,13},{16,9},{21,10},{32,25},{37,4},{49,24},{2,15},{38,41},{37,34},{33,6},{45,4},{18,18},{46,39},{12,24}}, {{6,1,2,9,4,10,0,11,5,13,3,8,12,7}})
+        TestCase({{19,13},{16,9},{21,10},{32,25},{37,4},{49,24},{2,15},{38,41},{37,34},{33,6},{45,4},{18,18},{46,39},{12,24}}, {6,1,2,9,4,10,0,11,5,13,3,8,12,7}),
+        TestCase({{46,9},{46,42},{30,46},{30,13},{30,24},{30,5},{30,21},{29,46},{29,41},{29,18},{29,16},{29,17},{29,5},{22,15},{22,13},{22,25},{22,49},{22,44}}, {14,5,12,3,0,13,10,11,9,6,4,15,8,1,17,2,7,16})
     };
 
     for (auto & t : test_cases) {
-        CHECK(sol.solve(std::get<0>(t)) == std::get<1>(t));
+        CHECK(sol.getOrder(std::get<0>(t)) == std::get<1>(t));
         std::cout << std::endl;
     }
 }
