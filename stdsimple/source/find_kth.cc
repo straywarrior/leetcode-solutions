71f21e07b4a725ffee40affd219674f98120fb61
StrayWarrior <i@straywarrior.com>
2023-07-11 17:55:18 +0800

    Add stdsimple find_kth

---
+++ stdsimple/source/find_kth.cc
@@ -0,0 +1,113 @@
+#include "catch/catch.hpp"
+
+#include <tuple>
+#include <string>
+#include <stack>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+namespace simplestd {
+
+// if not found, returns false
+std::pair<bool, int> find_majority(const std::vector<int>& arr) {
+    int candidate = 0;
+    int count = 0;
+    for (auto x : arr) {
+        if (count == 0) {
+            candidate = x;
+            ++count;
+        } else {
+            if (candidate == x) count += 1;
+            else count -= 1;
+        }
+    }
+    if (count > 0) {
+        return std::make_pair(true, candidate);
+    } else {
+        return std::make_pair(false, -1);
+    }
+}
+
+void max_heapify(std::vector<int>& arr, int i, int n) {
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int largest = i;
+    if (l < n && arr[l] > arr[largest]) {
+        largest = l;
+    }
+    if (r < n && arr[r] > arr[largest]) {
+        largest = r;
+    }
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        max_heapify(arr, largest, n);
+    }
+}
+
+void build_max_heap(std::vector<int>& arr, int n) {
+    for (int i = (n - 2) / 2; i >= 0; --i) {
+        max_heapify(arr, i, n);
+    }
+}
+
+// find top kth element
+int find_kth_element_by_heap(const std::vector<int>& arr, int k) {
+    std::vector<int> heap = arr;
+    build_max_heap(heap, heap.size());
+    int n = heap.size();
+    for (int i = 0; i < k - 1; ++i) {
+        std::swap(heap[0], heap[n - 1]);
+        max_heapify(heap, 0, n - 1);
+        --n;
+    }
+    return heap[0];
+}
+
+int find_kth_element(const std::vector<int>& arr, int k) {
+    return find_kth_element_by_heap(arr, k);
+}
+
+int find_kth_element_by_sort(const std::vector<int>& arr, int k) {
+    std::vector<int> arr_copy = arr;
+    std::sort(arr_copy.begin(), arr_copy.end());
+    std::reverse(arr_copy.begin(), arr_copy.end());
+    return arr_copy[k - 1];
+}
+
+}
+
+TEST_CASE("simplestd.find_majority", "base") {
+    using namespace std;
+    using TestCase = std::tuple<vector<int>, pair<bool, int>>;
+    std::vector<TestCase> test_cases {
+        TestCase { {}, {false, -1} },
+        TestCase { {0}, {true, 0} },
+        TestCase { {1,1,5,2,8,4}, {false, -1} },
+        TestCase { {1,1,2,3}, {false, -1}},
+        TestCase { {1,1,1,1,0,0}, {true, 1} }
+    };
+
+    for (auto& t : test_cases) {
+        const vector<int>& arr = get<0>(t);
+        CHECK(simplestd::find_majority(arr) == get<1>(t));
+    }
+}
+
+TEST_CASE("simplestd.find_kth_element", "base") {
+    using namespace std;
+    using TestCase = std::tuple<vector<int>, int>;
+    std::vector<TestCase> test_cases {
+        TestCase { {3,5,1,4,2,6}, 3 },
+        TestCase { {3,5,1,4,2,6,2}, 3 }
+    };
+
+    for (auto& t : test_cases) {
+        const vector<int>& arr = get<0>(t);
+        int k = get<1>(t);
+        CHECK(simplestd::find_kth_element(arr, k)
+                == simplestd::find_kth_element_by_sort(arr, k));
+    }
+}
